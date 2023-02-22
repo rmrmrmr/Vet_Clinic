6abf4227407581e0395c04ae1779125db0a66b9a
rmrmrmr <ricardomartineza77@gmail.com>
2023-02-21 18:02:02 -0600

    Insert animals

--- data.sql
+++ data.sql
@@ -4,3 +4,8 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VAL
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Gabumon', '2018-11-15', 2, true, 8.0);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Devimon', '2017-05-12', 5, true, 11.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Charmander', '2020-02-08', 0, false, 11.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Plantmon', '2021-11-15', 2, true, 5.7);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Squirtle', '1993-04-02', 3, false, 12.13);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Angemon', '2005-06-12', 0, true, 45.0);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight) VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
