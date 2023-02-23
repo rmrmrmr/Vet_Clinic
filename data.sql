ae846f6e41a4c3d91737ca2a5d9c74d3c1e37b85
rmrmrmr <ricardomartineza77@gmail.com>
2023-02-23 14:29:52 -0600

    delete empy lines

--- data.sql
+++ data.sql
@@ -74,12 +74,3 @@ INSERT INTO visits (animals_id, vets_id, date_of_visit)
   ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), '2020-08-03'),
   ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24'),
   ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
-
-
-
-
-
-
-
-
-  
